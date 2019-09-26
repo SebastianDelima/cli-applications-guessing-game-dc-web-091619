196a6db6dbd3b03bd21d4f18b3f0aa85a9332faa
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-26 15:42:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@ require'pry'
 
 def run_guessing_game
  random_number =  rand(6) + 1
+ puts "Guess number"
  input = gets.chomp
   if input == random_number 
-    binding.pry
   puts "You guessed the correct number!" 
    elsif input != random_number && input != 'exit'
   puts "Sorry! The computer guessed #{random_number}."
