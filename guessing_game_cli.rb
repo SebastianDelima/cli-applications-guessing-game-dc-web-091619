31410c702c79348cc233eff8cfc6e3afa763e314
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-26 15:14:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ require'pry'
 
 def run_guessing_game
  random_number =  rand(1..6) 
- input = gets.chomp
+ input = gets.chomp.to_i
 if input == 'exit'
   puts "Goodbye!"
   elsif input != random_number
