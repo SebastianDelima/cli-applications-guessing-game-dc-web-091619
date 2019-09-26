f2efedaccd503af5ffe8ffafbfb6bae9c31b0000
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-26 15:26:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ if input == 'exit'
   puts "Goodbye!"
    elsif input != random_number && input != 'exit'
   puts "Sorry! The computer guessed #{random_number}."
-  else 
+  elsif input == random_number 
   puts "You guessed the correct number!" 
  end
 end
