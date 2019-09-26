d616878d12216deb2fa893d45412df06c4790abc
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-26 15:48:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,6 @@ require'pry'
 def run_guessing_game
  random_number =  rand(6) + 1
  input = gets.chomp.to_i
- binding.pry
   if input == random_number 
   puts "You guessed the correct number!" 
    elsif input != random_number && input != 'exit'
